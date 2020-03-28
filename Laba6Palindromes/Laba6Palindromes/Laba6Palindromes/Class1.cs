42dd3d7874d3101d1b1f189610546e514295a9c3
NoLifeRTTS <62793620+NoLifeRTTS@users.noreply.github.com>
2020-03-28 19:14:25 +0700

    Add files via upload

---
+++ Laba6Palindromes/Laba6Palindromes/Laba6Palindromes/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba6Palindromes
+{
+    public static class PalSearch
+    {
+        public static bool Pal(this int number)
+        {
+            bool check = true;
+            int len = number.ToString().Length;
+            int c = len;
+            for(int i = 1; i <= len/2; i++)
+            {
+                if (number % 10 != number / (int)Math.Pow(10, c-1))
+                {
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    check = true;
+                    c -= 1;
+                    number /= 10;
+                    number %= (int)Math.Pow(10, c - 1);
+                    c = number.ToString().Length;
+                }
+            }
+            return check;
+        }
+    }
+}
---
+++ Laba6Palindromes/Laba6Palindromes/Laba6Palindromes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laba6Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(c.Pal());
+        }
+    }
+}
