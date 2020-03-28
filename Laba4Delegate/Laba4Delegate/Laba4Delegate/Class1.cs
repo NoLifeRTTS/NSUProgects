64c1325a70501010d6ee553f83e70657b435d9d8
NoLifeRTTS <62793620+NoLifeRTTS@users.noreply.github.com>
2020-03-28 18:17:07 +0700

    Add files via upload

---
+++ Laba4Delegate/Laba4Delegate/Laba4Delegate/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba4Delegate
+{
+    class Methods
+    {
+       public double Add(double x)
+        {
+            double ans = x + 5;
+            Console.WriteLine("Операция сложения:" + ans);
+            return ans;
+        }
+        public double Multiply(double x)
+        {
+            double ans = x * 7;
+            Console.WriteLine("Операция умножения:" + ans);
+            return ans;
+        }
+        public double Subtraction(double x)
+        {
+            double ans = x - 3;
+            Console.WriteLine("Операция вычитания:" + ans);
+            return ans;
+        }
+        public double Division(double x)
+        {
+            double ans = x / 3;
+            Console.WriteLine("Операция деления без остатка:" + ans);
+            return ans;
+        }
+        public double Pow(double x)
+        {
+            double ans = x * x;
+            Console.WriteLine("Операция квадрата:" + ans);
+            return ans;
+        }
+
+    }
+}
---
+++ Laba4Delegate/Laba4Delegate/Laba4Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Laba4Delegate
+{
+    class Program
+    {
+        private delegate double MyDelegate(double x);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDelegate[] del = new MyDelegate[10];
+            Random rnd = new Random();
+            Methods meth = new Methods();
+            for(int i = 0; i < 10; i++)
+            {
+                int a = Convert.ToInt32(rnd.Next(1, 5));
+                switch (a)
+                {
+                    case 1:
+                        del[i] = meth.Add;
+                        break;
+                    case 2:
+                        del[i] = meth.Multiply;
+                        break;
+                    case 3:
+                        del[i] = meth.Division;
+                        break;
+                    case 4:
+                        del[i] = meth.Subtraction;
+                        break;
+                    case 5:
+                        del[i] = meth.Pow;
+                        break;
+
+                }
+            }
+            double num;
+            Console.WriteLine("Введите число, над которым хотите проделать операции");
+            num = double.Parse(Console.ReadLine());
+            for(int j = 0; j < 10; j++)
+            {
+                num = del[j](num);
+
+            }
+            
+        }
+    }
+}
